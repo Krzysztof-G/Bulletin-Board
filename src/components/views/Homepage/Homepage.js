c19b56845b2da0a50748dfd62d44cebb59f51809
Krzysztof Gumuła <k.gumula@icloud.com>
2020-09-10 14:05:40 +0200

    improve server workspace

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import clsx from 'clsx';
+import clsx from "clsx";
 
-import { connect } from 'react-redux';
-import { getAllPublished, fetchPublished} from '../../../redux/postsRedux';
+import { connect } from "react-redux";
+import { getAll } from "../../../redux/postsRedux";
 
-import styles from './Homepage.module.scss';
+import styles from "./Homepage.module.scss";
 
-import { Link } from 'react-router-dom';
-import { Button, ListItem, List } from '@material-ui/core';
+import { Link } from "react-router-dom";
+import { Button, ListItem, List } from "@material-ui/core";
 
-const Component = ({className, children, posts, users}) => {
-
-  return(
+const Component = ({ className, children, posts, users }) => {
+  console.log(posts);
+  return (
     <div className={clsx(className, styles.root)}>
       <h2>Posts</h2>
-      <Button className={styles.addnewpost} component={Link} to={`/post/add`} variant="outlined" color="primary" >
+      <Button
+        className={styles.addnewpost}
+        component={Link}
+        to={`/post/add`}
+        variant="outlined"
+        color="primary"
+      >
         Add new post
       </Button>
       <List>
-        {posts.map(post => (
-          <ListItem className={styles.root} key={post.id} component={Link} to={`/post/${post.id}`}>
-            <Button className={styles.list}>
-              {post.title}
-            </Button>
+        {posts.map((post) => (
+          <ListItem
+            className={styles.root}
+            key={post.id}
+            component={Link}
+            to={`/post/${post.id}`}
+          >
+            <Button className={styles.list}>{post.title}</Button>
           </ListItem>
         ))}
       </List>
     </div>
   );
 };
-
-
-
 
 Component.propTypes = {
   children: PropTypes.node,
@@ -42,29 +48,11 @@ Component.propTypes = {
   users: PropTypes.array,
 };
 
-// const mapStateToProps = state => ({
-//   posts: getAll(state),
-//   users: getUsers(state),
-//   //   someProp: reduxSelector(state),
-// });
-
-// // const mapDispatchToProps = dispatch => ({
-// //   someAction: arg => dispatch(reduxActionCreator(arg)),
-// // });
-
-// const Container = connect(mapStateToProps)(Component);
-// // const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
-const mapStateToProps = state => ({
-  posts: getAllPublished(state),
+const mapStateToProps = (state) => ({
+  posts: getAll(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  fetchPublishedPosts: () => dispatch(fetchPublished()),
-});
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
+const Container = connect(mapStateToProps)(Component);
 
 export {
   //Component as Homepage,
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,15 +1,15 @@
 import Axios from 'axios';
 /* selectors */
-export const getAll = ({posts}) => posts.data;
-export const getPost = ({posts}, postID) => {
-  const post = posts.data.filter(post => post.id === parseInt(postID));
+export const getAll = ({ posts }) => posts.data;
+export const getPost = ({ posts }, postID) => {
+  const post = posts.data.filter((post) => post.id === parseInt(postID));
   return post[0];
 };
-export const getUsers = ({users}) => users;
+export const getUsers = ({ users }) => users;
 
 /* action name creator */
 const reducerName = 'posts';
-const createActionName = name => `app/${reducerName}/${name}`;
+const createActionName = (name) => `app/${reducerName}/${name}`;
 
 /* action types */
 const FETCH_START = createActionName('FETCH_START');
@@ -19,25 +19,24 @@ const ADD_POST = createActionName('ADD_POST');
 const EDIT_POST = createActionName('EDIT_POST');
 
 /* action creators */
-export const fetchStarted = payload => ({ payload, type: FETCH_START });
-export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
-export const fetchError = payload => ({ payload, type: FETCH_ERROR });
-export const addPost = payload => ({ payload, type: ADD_POST });
-export const editPost = payload => ({ payload, type: EDIT_POST });
-export const getAllPublished = ({posts}) => posts.data.filter(item => item.status === 'published');
+export const fetchStarted = (payload) => ({ payload, type: FETCH_START });
+export const fetchSuccess = (payload) => ({ payload, type: FETCH_SUCCESS });
+export const fetchError = (payload) => ({ payload, type: FETCH_ERROR });
+export const addPost = (payload) => ({ payload, type: ADD_POST });
+export const editPost = (payload) => ({ payload, type: EDIT_POST });
+export const getAllPublished = ({ posts }) =>
+  posts.data.filter((item) => item.status === 'published');
 
 /* thunk creators */
 export const fetchPublished = () => {
   return (dispatch, getState) => {
     dispatch(fetchStarted());
-
-    Axios
-      .get('http://localhost:8000/api/posts')
-      .then(res => {
+    Axios.get('http://localhost:8000/api/posts')
+      .then((res) => {
         dispatch(fetchSuccess(res.data));
         console.log(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         dispatch(fetchError(err.message || true));
       });
   };
@@ -77,10 +76,7 @@ export const reducer = (statePart = [], action = {}) => {
     case ADD_POST: {
       return {
         ...statePart,
-        data: [
-          ...statePart.data,
-          action.payload,
-        ],
+        data: [...statePart.data, action.payload],
       };
     }
     case EDIT_POST: {
